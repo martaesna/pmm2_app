a1ba9cd7b38814bdc0a808c17ab57aca96512ee3
Marta Estragués Navarro <marta.estragues@students.salle.url.edu>
2022-03-15 20:23:40 +0100

    All views fixed.

--- app/src/main/java/com/example/myapplication/ui/menu/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/ui/menu/MenuActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
@@ -29,8 +31,42 @@ public class MenuActivity extends AppCompatActivity {
         final Button exitButton = findViewById(R.id.sortir);
         final ImageButton configButton = findViewById(R.id.ButtonConfig);
         final ImageButton editButton = findViewById(R.id.BotonEdit);
+        editButton.setEnabled(false);
         final ImageButton deleteButton = findViewById(R.id.BotonDelete);
+        deleteButton.setEnabled(false);
         final ImageButton addButton = findViewById(R.id.BotonAdd);
+        addButton.setEnabled(false);
+        final View phoneButton = findViewById(R.id.telefon);
+        phoneButton.setEnabled(true);
+        phoneButton.setClickable(true);
+
+        final View purseButton = findViewById(R.id.cartera);
+        purseButton.setEnabled(true);
+        purseButton.setClickable(true);
+
+
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                phoneButton.setBackgroundResource(R.drawable.rectangle_clicked);
+                purseButton.setBackgroundResource(R.drawable.rectangle_login_textedit);
+                editButton.setEnabled(true);
+                deleteButton.setEnabled(true);
+                addButton.setEnabled(true);
+            }
+        });
+
+        purseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                purseButton.setBackgroundResource(R.drawable.rectangle_clicked);
+                phoneButton.setBackgroundResource(R.drawable.rectangle_login_textedit);
+                editButton.setEnabled(true);
+                deleteButton.setEnabled(true);
+                addButton.setEnabled(true);
+            }
+        });
+
 
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,5 +102,12 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    public static class MyClickListener2 implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            //do something
+        }
     }
 }
