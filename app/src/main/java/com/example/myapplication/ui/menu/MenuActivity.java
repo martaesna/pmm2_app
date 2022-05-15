911874bf8a08ba9e146d26db1e6b75ad9e44a443
Marta Estragués Navarro <marta.estragues@students.salle.url.edu>
2022-05-15 23:40:48 +0200

    Implemented: - Devices from menu connected to firebase - Arrow spinner

--- app/src/main/java/com/example/myapplication/ui/menu/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/ui/menu/MenuActivity.java
@@ -1,17 +1,27 @@
 package com.example.myapplication.ui.menu;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
+import com.example.myapplication.data.model.Account;
+import com.example.myapplication.data.model.Device;
 import com.example.myapplication.databinding.ActivityMenuBinding;
 import com.example.myapplication.ui.add.AddActivity;
 import com.example.myapplication.ui.chooseuser.ChooseUserActivity;
@@ -21,8 +31,11 @@ import com.example.myapplication.ui.edit.EditActivity;
 import com.example.myapplication.ui.help.HelpActivity;
 import com.example.myapplication.ui.login.LoginActivity;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuActivity extends AppCompatActivity {
     private ActivityMenuBinding binding;
@@ -35,6 +48,8 @@ public class MenuActivity extends AppCompatActivity {
         binding = ActivityMenuBinding.inflate(getLayoutInflater());
 
         String info = getIntent().getExtras().getString("user_info");
+        String[] user_info = info.split("/");
+        String username = user_info[1];
         String account = getIntent().getExtras().getString("account");
         int num_users = getIntent().getExtras().getInt("num_users");
         int accountID = getIntent().getExtras().getInt("accountID");
@@ -45,6 +60,59 @@ public class MenuActivity extends AppCompatActivity {
         final ImageButton editButton = findViewById(R.id.BotonEdit);
         final ImageButton deleteButton = findViewById(R.id.BotonDelete);
         final ImageButton addButton = findViewById(R.id.BotonAdd);
+
+
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("accounts");
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds: snapshot.getChildren()) {
+                    Account accountToFind = ds.getValue(Account.class);
+                    assert accountToFind != null;
+                    if (accountToFind.getName().equals(account)) {
+                        getIntent().putExtra("accountID", accountToFind.getAccountID());
+                        getIntent().putExtra("numDevices", accountToFind.getUsers().size());
+                        for (int i = 0; i < accountToFind.getUsers().size(); i++) {
+                            if (accountToFind.getUsers().get(i).getName().equals(username)) {
+                                getIntent().putExtra("userID", i);
+                                for (Device device: accountToFind.getUsers().get(i).getDevices()) {
+                                    if (device != null) {
+                                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                                                ViewGroup.LayoutParams.WRAP_CONTENT,
+                                                ViewGroup.LayoutParams.WRAP_CONTENT
+                                        );
+                                        params.setMargins(200, 100, 0, 0);
+
+                                        TextView textView = new TextView(getApplicationContext());
+                                        textView.setWidth(700);
+                                        textView.setHeight(240);
+                                        textView.setText(device.getObjectName());
+                                        textView.setBackgroundColor(Color.WHITE);
+                                        textView.setGravity(Gravity.CENTER);
+                                        textView.setTextColor(Color.BLUE);
+                                        textView.setTextSize(30);
+
+                                        LinearLayout ll = (LinearLayout)findViewById(R.id.menull);
+                                        ll.addView(textView, params);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                throw error.toException();
+            }
+        });
+
+
+
+
+
+
 
         ImageButton helpButton = findViewById(R.id.help);
 
